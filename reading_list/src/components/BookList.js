cb381207b2bad18e6d8f0252a04e1d49f988bef2
Pedro Lopez <pjlopezb@gmail.com>
2025-07-19 17:19:41 +0200

    Delete a document

--- reading_list/src/components/BookList.js
+++ reading_list/src/components/BookList.js
@@ -1,16 +1,21 @@
+import { db } from "../firebase/config";
+
+import { collection, doc, deleteDoc } from "firebase/firestore";
+
 export default function BookList({ books }) {
+	const handleClick = async (id) => {
+		await deleteDoc(doc(collection(db, "books"), id));
+	};
 
-  const handleClick = async (id) => {
-    console.log(id)
-  }
-
-  return (
-    <div className="book-list">
-      <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
-        ))}
-      </ul>
-    </div>
-  )
+	return (
+		<div className="book-list">
+			<ul>
+				{books.map((book) => (
+					<li key={book.id} onClick={() => handleClick(book.id)}>
+						{book.title}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
